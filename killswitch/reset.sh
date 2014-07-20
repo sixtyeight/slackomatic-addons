50ee11beb15da8590910e25a36521f1e7ff1e2d1
sixtyeight <pi@raspberrypi.(none)>
2014-07-20 01:27:25 +0000

    added browser close via cgi-bin on screeninvader

--- killswitch/reset.sh
+++ killswitch/reset.sh
@@ -5,6 +5,6 @@ killall killswitch.sh
 
 # close the browser, whatever it is showing
 
-wget -O /dev/null "http://10.20.30.40/#/controll/browserClose" > /dev/null 2>&1
+wget -O /dev/null "http://10.20.30.40/cgi-bin/close/browser" > /dev/null 2>&1
 
 exit 0 
