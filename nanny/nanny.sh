951d695889dc5e05121f3fff24e989e859d652eb
sixtyeight <pi@raspberrypi.(none)>
2015-03-08 00:13:51 +0100

    cleanup will trigger chinese sweatshop

--- nanny/nanny.sh
+++ nanny/nanny.sh
@@ -2,3 +2,5 @@
 
 # post the cleanup info to the screeninvader
 wget -O /dev/null "http://10.20.30.40/cgi-bin/show?http://10.20.30.90/cleanup_reminder.jpg" > /dev/null 2>&1
+wget -O /dev/null "http://10.20.30.90/slackomatic/rooms/lounge/lighting/chinese_sweatshop" > /dev/null 2>&1
+
