0921d849369861d78adf71f2224a8e959f7d0cfb
sixtyeight <pi@raspberrypi.(none)>
2015-03-14 00:28:00 +0100

    recent screeninvader playlist activity will prevent powerdown

--- killswitch/killswitch.sh
+++ killswitch/killswitch.sh
@@ -3,11 +3,23 @@
 DELAY_IN_SECONDS=$1
 POWERDOWN_CALLBACK_URI=$2
 
-# post the power down warning to the screeninvader
-wget -O /dev/null "http://10.20.30.40/cgi-bin/show?http://10.20.30.90/power_down_warning.png" > /dev/null 2>&1
+PLAYLIST=`wget -qO- "http://10.20.30.40/cgi-bin/playlist_last"`
+if ! [[ "$PLAYLIST" =~ ^[0-9]+$ ]]; then PLAYLIST=0; fi
+CURTIME=`date +%s`
+DIFF=$(($CURTIME-$PLAYLIST))
+if [ $DIFF -gt 1800 ]
+then
+  # post the power down warning to the screeninvader
+  wget -O /dev/null "http://10.20.30.40/cgi-bin/show?http://10.20.30.90/power_down_warning.png" > /dev/null 2>&1
 
-echo "powering down devices at '${POWERDOWN_CALLBACK_URI}' in ${DELAY_IN_SECONDS} seconds"
-sleep ${DELAY_IN_SECONDS}
+#  /home/pi/slackomatic-addons/homematic/power0.sh 1 
 
-echo "invoking callback '${POWERDOWN_CALLBACK_URI}'"
-wget -O /dev/null "${POWERDOWN_CALLBACK_URI}" > /dev/null 2>&1
+  echo "powering down devices at '${POWERDOWN_CALLBACK_URI}' in ${DELAY_IN_SECONDS} seconds"
+  sleep ${DELAY_IN_SECONDS}
+  
+#  /home/pi/slackomatic-addons/homematic/power0.sh 0 
+
+  echo "invoking callback '${POWERDOWN_CALLBACK_URI}'"
+  wget -O /dev/null "${POWERDOWN_CALLBACK_URI}" > /dev/null 2>&1
+fi
+
