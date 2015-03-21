734c542233d8778695dd8f39294f323da5b3a48a
sixtyeight <pi@raspberrypi.(none)>
2015-03-21 01:39:18 +0100

    using images from nodejs frontend

--- killswitch/killswitch.sh
+++ killswitch/killswitch.sh
@@ -10,7 +10,7 @@ DIFF=$(($CURTIME-$PLAYLIST))
 if [ $DIFF -gt 1800 ]
 then
   # post the power down warning to the screeninvader
-  wget -O /dev/null "http://10.20.30.40/cgi-bin/show?http://10.20.30.90/power_down_warning.png" > /dev/null 2>&1
+  wget -O /dev/null "http://10.20.30.40/cgi-bin/show?http://10.20.30.90/img/power_down_warning.png" > /dev/null 2>&1
 
 #  /home/pi/slackomatic-addons/homematic/power0.sh 1 
 
